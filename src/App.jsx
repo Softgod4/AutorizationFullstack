e5aeb5ea0458758ce79d6eb5788ffef5268c380e
Softgod <softgod2005@gmail.com>
2023-05-21 22:35:53 +0300

    Добавлен код

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="Autorise">
+        <div className="PersonalInfo">
+          <p>PERSONAL INFO</p>
+          <div className="Field">
+            <p>Name*</p>
+            <input type="text" />
+          </div>
+          <div className="Field"></div>
+          <div className="Field"></div>
+          <div className="Field"></div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
